e7030aea3fd7e7f5e4e9deb019d4b80829050731
bulent <bulenterdem111@gmail.com>
2022-12-20 23:20:00 +0300

    2.gun

--- src/test/java/day02_driverMethodlari/C05_WindowHandleDegeri.java
+++ src/test/java/day02_driverMethodlari/C05_WindowHandleDegeri.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class C05_WindowHandleDegeri {
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("wedriver.chrome.driver","src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.amazon.com");
--- src/test/java/day02_driverMethodlari/C07_DriverFindEementMethodu.java
+++ src/test/java/day02_driverMethodlari/C07_DriverFindEementMethodu.java
@@ -1,29 +1,30 @@
 package day02_driverMethodlari;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C06_DriverManageMethodlari {
+public class C07_DriverFindEementMethodu {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        // gittigimiz sayfa acilinciya veya aradigimiz web element bulununcaya kadar
-        //driverin bekleyebilecigi  max sureyi belirler
 
-        // bu dortlu her test methodumuzun basina yazilacak
+         driver.get("http://www.amazon.com");
 
-        // driver.get("http://www.amazon.com");
-        // driver.get("http://www.wisequarter.com");
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
 
+        aramaKutusu.sendKeys("java");
+        aramaKutusu.clear();
+        Thread.sleep(3000);
 
-
-
-
+        aramaKutusu.sendKeys("Nutella");
+        aramaKutusu.submit();
 
 
 
